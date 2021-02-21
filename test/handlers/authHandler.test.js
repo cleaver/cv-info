84e8785cca12c9e45ee9cbe0b35155ec5e1f32d5
Cleaver Barnes <cbarnes@cleaver.ca>
2021-02-21 13:13:32 -0500

    add tests for authHandler

---
+++ test/handlers/authHandler.test.js
@@ -0,0 +1,27 @@
+const authHandler = require('../../src/handlers/authHandler');
+const jwt = require('jsonwebtoken');
+
+jest.mock('jsonwebtoken');
+
+describe('authHandler tests', () => {
+  process.env.AUTH_USERNAME = 'foo';
+  beforeEach(() => {});
+
+  test('valid token, good username', () => {
+    jwt.verify.mockReturnValue({ username: 'foo' });
+    expect(authHandler('valid')).resolves.toBe('foo');
+  });
+
+  test('valid token, bad username', () => {
+    jwt.verify.mockReturnValue({ username: 'bar' });
+    console.log('AUTH_USERNAME', process.env.AUTH_USERNAME);
+    expect(authHandler('valid')).rejects.toMatch('error');
+  });
+
+  test('invalid token', () => {
+    jwt.verify.mockImplementation((scalar) => {
+      throw new Error();
+    });
+    expect(authHandler('invalid')).rejects.toMatch('error');
+  });
+});
